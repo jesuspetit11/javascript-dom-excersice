bb0142357bb1ae5ca2ce9f20941d84e54492ce06
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-09-16 20:09:52 -0400

    actualizando contenido del archivo js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const caja = document.createElement("div");
+window.onload = inicio(); //preguntar por esto a Juan
+
+function inicio() {
+    document.querySelector("body").insertAdjacentHTML("beforeend",`
+        <button id="boton1" class="boton">Botón 1</button>
+        <button id="boton2" class="boton">Botón 2</button>
+        <button id="boton3" class="boton">Botón 3</button>
+        <div id="imagen"><img src="./img/HTML5_Badge.svg" alt=""></div>`)
+}
 
